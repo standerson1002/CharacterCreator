f7d3e90c2e6d15abc5a3f2d31d8d915113ff0f8a
Stan Anderson <standerson1002@gmail.com>
2025-06-03 14:04:06 -0500

    Small Web UI Updates

--- CharacterCreator/MVCCharacterCreator/obj/Debug/net8.0/MVCCharacterCreator.AssemblyInfo.cs
+++ CharacterCreator/MVCCharacterCreator/obj/Debug/net8.0/MVCCharacterCreator.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCCharacterCreator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+249b4e0367e2e192dbd0f145112f0d902705ae42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d6ea4304176a90e999e1d94820486d91787d6eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCCharacterCreator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCCharacterCreator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
